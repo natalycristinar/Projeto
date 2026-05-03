08cb9d6f49a4b773847edbd6e0fe4a1f6d63cba3
unknown <nicollasalmeidacontato@gmail.com>
2026-05-03 14:03:05 -0300

    Adiciona atributo dataRealizacao na classe AcaoInclusiva

--- src/AcaoInclusiva.java
+++ src/AcaoInclusiva.java
@@ -1,12 +1,22 @@
 public class AcaoInclusiva {
     private String titulo;
     private String descricao;
+    private String dataRealizacao;
 
     public AcaoInclusiva(String titulo, String descricao) {
         this.titulo = titulo;
         this.descricao = descricao;
     }
-
+    
+    public String getDataRealizacao() {
+    	return dataRealizacao;
+    }
+    
+    public void setDataRealizacao(String dataRealizacao) {
+    	this.dataRealizacao = dataRealizacao;
+    }
+    
+    	
     public String getTitulo() {
         return titulo;
     }
